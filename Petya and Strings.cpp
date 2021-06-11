2ad47c0c47f82b11aace55a0b216e9f74816a46e
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-06-11 20:46:23 +0600

    Update Petya and Strings.cpp

--- Petya and Strings.cpp
+++ Petya and Strings.cpp
@@ -1,24 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+int main()
+{
+    long long i;
+    string f,s;
+    while(cin>>f>>s)
+    {
+        long sum1=0,sum2=0;
+        for(i=0; i<f.size(); i++)
+        {
+            if(f[i]>='A'&&f[i]<='Z')
+                f[i]=f[i]-'A'+97;
 
-string low(string s){
-    string ans;
-    for (int i=0;i<s.size();++i){
-        ans+=tolower(s[i]);
+            if(s[i]>='A'&&s[i]<='Z')
+                s[i]=s[i]-'A'+97;
+        }
+        for(i=0; i<s.size(); i++)
+        {
+            if(f[i]>s[i])
+                {
+                    cout<<"1\n";
+                    return 0;}
+
+            if(s[i]>f[i])
+                {
+                    cout<<"-1\n";
+                    return 0;
+                }
+
+        }
+        cout<<"0\n";
     }
-    return ans;
-}
-
-bool lexi(string s1,string s2){
-    for (int i=0;i<s1.size();++i){
-        if(tolower(s1[i])>tolower(s2[i])) return 1;
-    }
-}
-
-int main(){
-    string s1,s2;
-    int count;
-    cin>>s1>>s2; 
-    if (low(s1)==low(s2)) cout<<"0";
-    else{lexi(s1,s2)?cout<<"1":cout<<"-1";}
+    return 0;
 }
