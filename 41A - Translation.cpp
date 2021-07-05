5fb0ace94f8a9891e21dec87b0d2a4ca7894f94c
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-07-05 13:35:42 +0600

    Create 41A - Translation.cpp

---
+++ 41A - Translation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s, t;
+	cin >> s >> t;
+	reverse(t.begin(), t.end());
+	if (t == s) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+
+}
